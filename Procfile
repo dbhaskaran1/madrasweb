65f8f12e4432b1037d2ee7a074a3829951440242
Deepak Bhaskaran <dbhaskaran@pindropsecurity.com>
2015-09-15 12:07:55 -0400

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python madrasweb.py
---
+++ madrasweb.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_url_path="/static", static_folder="static")
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/blog.html")
+def blog():
+    return render_template('blog.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
